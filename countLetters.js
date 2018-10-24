8ff7689ccd59984f150715238ee5b20e049f654a
Ying Dong <dongyingname@yahoo.com>
2018-10-24 21:08:43 +0000

    changed a bit

--- countLetters.js
+++ countLetters.js
@@ -1,18 +1,16 @@
-
-
-
 function countLetters(input) {
   function reformatt(str){
     var noSpace = str.split(" ").join('').toLowerCase();
     return noSpace;
   }
+  var newString = reformatt(input);
   var result = [];
   var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-  for (var i = 0; i < input.length; i++){
+  for (var i = 0; i < newString.length; i++){
     for (var j = 0; j < letters.length; j ++){
       if(input[i] === letters[j]){
-        if (result[input[i]] === undefined){
-          result[input[i]] = 1;
+        if (result[newString[i]] === undefined){
+          result[newString[i]] = 1;
         }
         else {
           result[input[i]] += 1;
